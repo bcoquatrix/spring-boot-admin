8e5e9f6eb4273e41cb3aa0112539cb1cbf38936c
Bruno COQUATRIX <bruno.coquatrix@gmail.com>
2022-10-23 19:43:56 +0200

    Initialize the project

---
+++ client1/src/test/java/fr/poc/client1/Client1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.poc.client1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Client1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
